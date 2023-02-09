daf8fe32838b592855ceda580b6d8b48e0418369
pChou19 <1326306+pChou19@users.noreply.github.com>
2023-02-08 21:01:42 -0500

    Change to calling WMATA api

--- netlify/functions/fetch-wmata/fetch-wmata.js
+++ netlify/functions/fetch-wmata/fetch-wmata.js
@@ -1,9 +1,11 @@
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://cat-fact.herokuapp.com/facts";
+const { API_KEY } = process.env.WMATA_KEY
+const API_ENDPOINT = "https://api.wmata.com/StationPrediction.svc/json/GetPrediction/";
+const stationCode = event.queryStringParameters && event.queryStringParameters.name;
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
+  return fetch(API_ENDPOINT+stationCode, { headers: { 'api_key': API_KEY } })
     .then((response) => response.json())
     .then((data) => ({
       statusCode: 200,
